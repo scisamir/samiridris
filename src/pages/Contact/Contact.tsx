14addd5f7397400a05587bb67ff77d09acf5635b
Samir Idris <samzidris11@gmail.com>
2023-11-10 13:35:13 +0100

    change header

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -52,70 +52,65 @@ const Contact = () => {
     }
 
     return (
-        <>
-            <section id="contact">
-                <h2>Contact Me</h2>
-                <ToastContainer theme='dark' position='bottom-center' />
-                <div>
-                    <div id='contactInfo'>
-                        <h3>Contact Info</h3>
+        <section id="contact">
+            <h2>Contact Me</h2>
+            <ToastContainer theme='dark' position='bottom-center' />
+            <div>
+                <div id='contactInfo'>
+                    <h3>Contact Info</h3>
+                    <ul>
+                        <li>
+                            <BsFillPersonLinesFill fontSize="2.7rem" />
+                            <div>
+                                <p>Name:</p>
+                                <p className='contactDetails'>Samir Idris</p>
+                            </div>
+                        </li>
+                        <li>
+                            <CiLocationOn fontSize="3rem" />
+                            <div>
+                                <p>Location:</p>
+                                <p className='contactDetails'>Abuja, Nigeria</p>
+                            </div>
+                        </li>
+                        <li>
+                            <AiOutlineMail fontSize="2.7rem" />
+                            <div>
+                                <p>Email:</p>
+                                <p className='contactDetails'>samzidris11@gmail.com</p>
+                            </div>
+                        </li>
+                    </ul>
+                    <div id='socials'>
+                        <p>Social Handles</p>
                         <ul>
-                            <li>
-                                <BsFillPersonLinesFill fontSize="2.7rem" />
-                                <div>
-                                    <p>Name:</p>
-                                    <p className='contactDetails'>Samir Idris</p>
-                                </div>
-                            </li>
-                            <li>
-                                <CiLocationOn fontSize="3rem" />
-                                <div>
-                                    <p>Location:</p>
-                                    <p className='contactDetails'>Abuja, Nigeria</p>
-                                </div>
-                            </li>
-                            <li>
-                                <AiOutlineMail fontSize="2.7rem" />
-                                <div>
-                                    <p>Email:</p>
-                                    <p className='contactDetails'>samzidris11@gmail.com</p>
-                                </div>
-                            </li>
+                            <li><a href="https://linkedin.com/in/scisamir" target="_blank">
+                                <AiFillLinkedin color="#0072b1" fontSize="3rem" />
+                            </a></li>
+                            <li><a href="https://github.com/scisamir" target="_blank">
+                                <AiFillGithub color="#000000" fontSize="3rem" />
+                            </a></li>
+                            <li><a href="https://twitter.com/ScientistSamir" target="_blank">
+                                <FaXTwitter color="#000000" fontSize="2.5rem" />
+                            </a></li>
+                            <li><a href="mailto:samzidris11@gmail.com">
+                                <img src={gmail} alt="gmail logo" style={{height: "2.1rem"}} />
+                            </a></li>
                         </ul>
-                        <div id='socials'>
-                            <p>Social Handles</p>
-                            <ul>
-                                <li><a href="https://linkedin.com/in/scisamir" target="_blank">
-                                    <AiFillLinkedin color="#0072b1" fontSize="3rem" />
-                                </a></li>
-                                <li><a href="https://github.com/scisamir" target="_blank">
-                                    <AiFillGithub color="#000000" fontSize="3rem" />
-                                </a></li>
-                                <li><a href="https://twitter.com/ScientistSamir" target="_blank">
-                                    <FaXTwitter color="#000000" fontSize="2.5rem" />
-                                </a></li>
-                                <li><a href="mailto:samzidris11@gmail.com">
-                                    <img src={gmail} alt="gmail logo" style={{height: "2.1rem"}} />
-                                </a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div id='contactForm'>
-                        <form onSubmit={handleSubmit}>
-                            <h3>Message Me</h3>
-                            <input required type="text" name="name" id="name" placeholder="Enter your Name" value={formState.name} onChange={handleInputChange} />
-                            <input required type="email" name="email" id="email" placeholder="Enter your Email" value={formState.email} onChange={handleInputChange} />
-                            <input type="text" name="title" id="title" placeholder="Enter message Title" value={formState.title} onChange={handleInputChange} />
-                            <textarea name="message" id="message" placeholder="Enter your message..." value={formState.message} onChange={handleInputChange}></textarea>
-                            <button type="submit">Send Message</button>
-                        </form>
                     </div>
                 </div>
-            </section>
-            <section id="copyright">
-                Made with <span>&#10084;</span> by Samir Idris
-            </section>
-        </>
+                <div id='contactForm'>
+                    <form onSubmit={handleSubmit}>
+                        <h3>Message Me</h3>
+                        <input required type="text" name="name" id="name" placeholder="Enter your Name" value={formState.name} onChange={handleInputChange} />
+                        <input required type="email" name="email" id="email" placeholder="Enter your Email" value={formState.email} onChange={handleInputChange} />
+                        <input type="text" name="title" id="title" placeholder="Enter message Title" value={formState.title} onChange={handleInputChange} />
+                        <textarea name="message" id="message" placeholder="Enter your message..." value={formState.message} onChange={handleInputChange}></textarea>
+                        <button type="submit">Send Message</button>
+                    </form>
+                </div>
+            </div>
+        </section>
     );
 }
 
